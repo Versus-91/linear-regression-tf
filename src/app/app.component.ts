90cc61775a6e2c3dcf797ab332042771614d55a0
Ghader <gharibyan.ghader@gmail.com>
2023-09-29 22:08:37 +0200

    adding lodash and cvs parser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,8 +17,8 @@ export class AppComponent implements OnInit {
     this.linearmodel.add(tf.layers.dense({ units: 1, inputShape: [1] }));
     this.linearmodel.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
 
-    const xs = tf.tensor1d([1,2]);
-    const ys = tf.tensor1d([2,4]);
+    const xs = tf.tensor1d([1, 2]);
+    const ys = tf.tensor1d([2, 4]);
     await this.linearmodel.fit(xs, ys);
     console.log('training is complete');
   }
