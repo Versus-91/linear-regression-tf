0eb667041def61bd3ceddf5aa1af4aef3a875881
Ghader <gharibyan.ghader@gmail.com>
2023-10-06 00:56:19 +0200

    loading data and scatterpot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import * as tf from '@tensorflow/tfjs';
+declare let tfvis: any
+interface Car {
+  Horsepower: any;
+  Name: string,
+  Miles_per_Gallon: number
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,8 +16,34 @@ export class AppComponent implements OnInit {
   title = 'linear-regression-tf';
   linearmodel: tf.Sequential = tf.sequential();
   prediction: any = 0;
+  cars: any = []
+  constructor(private http: HttpClient) {
+  }
+
   ngOnInit() {
-    this.trainNewModel();
+    // this.trainNewModel();
+    this.loadCars()
+  }
+  loadCars() {
+    this.http.get<Car[]>('https://storage.googleapis.com/tfjs-tutorials/carsData.json').subscribe((res) => {
+      this.cars = res.map((car) => ({
+        mpg: car.Miles_per_Gallon,
+        horsepower: car.Horsepower
+      }))
+      const values = this.cars.map((d: any) => ({
+        x: d.horsepower,
+        y: d.mpg,
+      }));
+      tfvis.render.scatterplot(
+        { name: 'Horsepower v MPG' },
+        { values: values },
+        {
+          xLabel: 'Horsepower',
+          yLabel: 'MPG',
+          height: 300
+        }
+      );
+    })
   }
   async trainNewModel() {
     this.linearmodel = tf.sequential();
